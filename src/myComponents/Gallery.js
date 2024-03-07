5ece4b23673b8eef82bd2e69efcdcd4ad1f79b0e
Dmytro Puntus <dmytro.puntus@digitecgalaxus.ch>
2024-03-07 13:47:46 +0100

    + design a little bit

--- src/myComponents/Gallery.js
+++ src/myComponents/Gallery.js
@@ -4,12 +4,12 @@ import './Gallery.css';
 // Create a context containing all .jpg files in the photos directory
 const imageContext = require.context('../photos', true, /\.jpg$/)
 
-export default function Gallery(){
+export default function Gallery() {
     const images = imageContext.keys().map((image, index) => {
         // The imageContext function returns the URL of the image
         const imageUrl = imageContext(image);
         // The image path can be used as a unique key, and in the alt attribute
-        return <img key={image} src={imageUrl} alt={image} className="image"/>;
+        return <img key={image} src={imageUrl} alt={image} className="image" />;
     });
 
     return (
@@ -17,4 +17,4 @@ export default function Gallery(){
             {images}
         </div>
     );
-  }
+}
--- src/myComponents/SearchForm.js
+++ src/myComponents/SearchForm.js
@@ -1,34 +1,36 @@
 import React from "react";
 import "./SearchForm.css";
 
-export default function SearchForm(){
-    return(
-      <form onSubmit={search} className="search-form">
-        <input name="query"/>
-        <button type="submit">Search</button>
-        <div className="radio-input-container">
-            <div className="radio-container">
-            <input type="radio" id="summer" name="season" value="summer"/>
-            <label for="summer">Summer</label>
+export default function SearchForm() {
+    return (
+        <form onSubmit={search} className="search-form">
+            <div className="search-container">
+                <input name="query" />
+                <button type="submit" className="button">Search</button>
             </div>
-            <div className="radio-container">
-            <input type="radio" id="spring" name="season" value="spring"/>
-            <label for="summer">Spring</label>
+            <div className="radio-input-container">
+                <div className="radio-container">
+                    <input type="radio" id="summer" name="season" value="summer" />
+                    <label for="summer">Summer</label>
+                </div>
+                <div className="radio-container">
+                    <input type="radio" id="spring" name="season" value="spring" />
+                    <label for="summer">Spring</label>
+                </div>
+                <div className="radio-container">
+                    <input type="radio" id="autumn" name="season" value="autumn" />
+                    <label for="summer">Autumn</label>
+                </div>
+                <div className="radio-container">
+                    <input type="radio" id="winter" name="season" value="winter" />
+                    <label for="summer">Winter</label>
+                </div>
             </div>
-            <div className="radio-container">
-            <input type="radio" id="autumn" name="season" value="autumn"/>
-            <label for="summer">Autumn</label>
-            </div>
-            <div className="radio-container">
-            <input type="radio" id="winter" name="season" value="winter"/>
-            <label for="summer">Winter</label>
-            </div>
-        </div>
-      </form>
+        </form>
     );
-  }
+}
 
-  function search(formData) {
+function search(formData) {
     const query = formData.get("query");
     alert(`You searched for '${query}'`);
-  }
+}
