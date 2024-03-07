66e4ee7bca87439c90097b6222a20da554aa50d6
Dmytro Puntus <dmytro.puntus@digitecgalaxus.ch>
2024-03-07 10:27:20 +0100

    + searchForm as separated file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchForm from './myComponents/SearchForm';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (
     <div class="container">
-    {
-      // comment 
-    }
-      <MyForm />
+      <SearchForm />
     </div>
   );
 }
 
-function MyForm(){
-  return(
-    <form>
-    <label for="fname">First name:</label><br/>
-    <input type="text" id="fname" name="fname"/><br/>
-    <label for="lname">Last name:</label><br/>
-    <input type="text" id="lname" name="lname"/>
-    </form>
-  );
-}
+
+
+
---
+++ src/myComponents/SearchForm.js
@@ -0,0 +1,24 @@
+export default function SearchForm(){
+    return(
+      <form onSubmit={search}>
+        <input name="query"/>
+        <button type="submit">Search</button>
+        <input type="radio" id="summer" name="season" value="summer"/>
+        <label for="summer">Summer</label>
+  
+        <input type="radio" id="autumn" name="season" value="autumn"/>
+        <label for="autumn">Autumn</label>
+  
+        <input type="radio" id="winter" name="season" value="winter"/>
+        <label for="winter">Winter</label>
+  
+        <input type="radio" id="spring" name="season" value="spring"/>
+        <label for="spring">Spring</label>
+      </form>
+    );
+  }
+
+  function search(formData) {
+    const query = formData.get("query");
+    alert(`You searched for '${query}'`);
+  }
